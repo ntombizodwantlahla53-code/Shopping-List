338cd7486132c773be57c8aa0faab23ca65f486c
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-19 22:37:12 +0200

    added second landing page

--- src/Pages/Landingpage/Landingpage.tsx
+++ src/Pages/Landingpage/Landingpage.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Text } from '../../components/Text/Text'
+import style from './Landingpage.module.css'
+import { CiShop } from "react-icons/ci";
+import { Buttons } from '../../components/Buttons/Button';
 
 export const Landingpage = () => {
   return (
-    <div>Landingpage</div>
+    <div className={style.land}>
+      
+        <div className={style.headingg}>
+            <h1 className={style.heading}>S-list </h1>
+            <div className={style.icon}><CiShop/></div>
+            <h2 className={style.welcome} >Create and share</h2>
+            <p className={style.paragraph}>Share shopping list with family<br/>and friends.</p>
+        </div>
+        <Link to="/login"><Buttons type="submit"
+                    label ="Next"
+                    icon={< CiShop/>}
+                    variant="inputting"/></Link>
+       
+    </div>
   )
 }
