043330c4393f768d25c122f5b1e68a48214b44bd
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-19 22:32:13 +0200

    added Firstlanding page

---
+++ src/Pages/FirstLanding/FirstLanding.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Text } from '../../components/Text/Text'
+import style from './FirstLanding.module.css'
+import { CiShop } from "react-icons/ci";
+import { Link } from 'react-router-dom';
+import { Buttons } from '../../components/Buttons/Button';
+import { IoIosAddCircle } from "react-icons/io";
+
+export const FirstLanding = () => {
+  return (
+    <div className={style.landing} >
+      <div className={style.skip}>
+        <Link to = "/login" >Skip</Link>
+      </div>
+            <h1 className={style.heading}>S-list </h1>
+           
+            <div className={style.icon}><CiShop/></div>
+            <h2 className={style.welcome}>Welcome to S-list</h2>
+            <div className={style.paragraph}>
+            <Text variant="p">Make your shopping the easiest and<br/>
+             fastest, in advance by making list of<br/>
+              your products with S-LIST.</Text></div>
+      <div className={style.nextBTN}>
+        <Link to="/landing2"><Buttons type="submit"
+                    label ="Next"
+                    icon={<IoIosAddCircle />}
+                    variant="inputting"/></Link></div>
+       
+    </div>
+  )
+}
