9b87338ac9b183d39405ea40e8550b4d30ee7957
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-19 22:34:28 +0200

    added search icon,profile and logout button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 import { Text } from '../Text/Text';
 import styles from './Navbar.module.css'
-import { FiCoffee } from "react-icons/fi";
 import { FaSearch } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { CgProfile } from "react-icons/cg";
