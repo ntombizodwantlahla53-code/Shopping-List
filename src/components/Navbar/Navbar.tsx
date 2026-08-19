ca143888f09413554ef88890a65b5db0a76bff98
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-19 22:23:26 +0200

    added search icon,profile and logout button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,15 +3,24 @@ import styles from './Navbar.module.css'
 import { FiCoffee } from "react-icons/fi";
 import { FaSearch } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { CgProfile } from "react-icons/cg";
+import { IoIosLogOut } from "react-icons/io";
 
 
 export const Navbar =() => {
 
   return (
     <nav>
-        <div className={styles.content}>
-            < FaSearch/>
-           
+        <div className={styles.content}>    
+            <div className={styles.heading}>
+          <FaSearch/>
+          </div>
+
+            
+            <div className={styles.links}>
+              <Link to = "/profile" ><Text><button className={styles.sNow}>Profile <CgProfile/></button></Text></Link>
+            <Link to = "/" ><Text><button className={styles.sNow}>Log Out <IoIosLogOut/></button></Text></Link>  
+             </div>
         </div>
 </nav>
   )}
