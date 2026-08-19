d5439ab5e903a6fb605742910a71e858391685b3
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-19 22:47:29 +0200

    added Router and updated app

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { Login } from './Pages/Login/Login'
 import React, { useState, useEffect } from "react";
 import { Register } from './Pages/Register/Register'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Landing } from './Pages/Landing/Landing';
+import { FirstLanding } from './Pages/FirstLanding/FirstLanding';
 import { Landingpage } from './Pages/Landingpage/Landingpage';
+import { Home } from './Pages/Home/Home';
+import { Profile } from './Pages/Profile/Profile';
+
 
 
 function App() {
@@ -14,10 +17,13 @@ function App() {
     <>
     <Router>
       <Routes>
-        <Route path = "/" element={<Login/>}/>
+        <Route path = "/" element={<FirstLanding/>}/>
+        <Route path = "/landing2" element={<Landingpage/>}/>
+        <Route path = "/home" element={<Home/>}/>
+        <Route path = "/login" element={<Login/>}/>
         <Route path = "/register" element={<Register/>}/>
-        <Route path = "/register" element={<Landing/>}/>
-        <Route path = "/register" element={<Landingpage/>}/>
+        <Route path = "/profile" element={<Profile/>}/>
+        
         </Routes></Router>  
     
     
