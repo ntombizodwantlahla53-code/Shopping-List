73045795119fc21326a3643dddd5f35418ac9f03
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-08-20 15:28:12 +0200

    added home reducer and register

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import loginReducer from './../Features/login'
+import loginReducer from '../components/Features/login'
+import registerReducer from '../components/Features/register'
+import homeReducer from '../components/Features/list'
 
 export const store = configureStore({
     reducer : {
-        logins : loginReducer,
+        register : registerReducer,
+        login : loginReducer,
+        home : homeReducer,
     }
 })
 
 export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
