a3e6d5f8c60dc9343f948434eda4cf3bca3eb701
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-23 23:06:42 +0200

    updated main

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -7,8 +7,12 @@ import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../../Redux/store";
 import { Link } from 'react-router-dom';
 
+
 export const Home = () => {
-  
+  const details = useSelector((state: RootState) => state.main.details)
+    if (!details) {
+      return <p>No userlogged in</p>
+    }
   return (
     <div className={style.home} >
     <Navbar/>
@@ -18,6 +22,10 @@ export const Home = () => {
     <h2 className={style.list}>Your List is Empty<br/></h2>
     <p>create/add list to your trolley for an<br/> easier shopping experience</p>
     </div>
+    <p>Catergory: {details.catergory}</p>
+      <p>Name:{details.name}</p>
+      <p>Note:{details.note}</p>
+      <p>Quantity:{details.quantity}</p>
     <Link to="/main"><Buttons type="submit"
     label ="Add List"
     icon={<IoIosAddCircle />}
