c4fca313cc9a2f4882d4e47e8d6719729cfd5153
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-08-25 15:35:04 +0200

    added new slice

---
+++ src/components/Features/items.ts
@@ -0,0 +1,70 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface Main {
+    catergory: string;
+
+}
+
+interface  Mainee{
+   details :{
+    catergory: string;
+   },
+   links:Main[];
+  loading: boolean;
+  error: string | null;
+};
+const initialState: Mainee = {
+  details: {
+    catergory: "",
+  },
+  links:[],
+  loading: false,
+  error: null,
+};
+
+export const fetchList = createAsyncThunk('list/fetchList', async(main:Main, thunkAPI)=>{
+try {
+  const response = await fetch("http://localhost:3000/main", {
+    method: "POST",
+    headers: {"Content-Type": "application/json",},
+    body: JSON.stringify(main),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to add items");
+  }
+  return await response.json();
+} catch (error) {
+  return thunkAPI.rejectWithValue(
+    error instanceof Error ? error.message : "Something went wrong"
+  );
+}
+}
+);
+export const mainSlice = createSlice({
+  name: "list",
+  initialState,
+  reducers: {
+    lists: (state, action: PayloadAction<Partial<Main>>) => {
+          state.details={...state.details, ...action.payload,};
+        },
+    },
+ 
+extraReducers: (builder) =>{
+  builder.addCase(fetchList.pending, (state) =>{
+    state.loading = true
+    state.error = null;
+  })
+  .addCase(fetchList.fulfilled, (state, action) =>{
+    state.loading= false
+    state.details = action.payload;
+    state.links.push(action.payload);
+})
+  .addCase(fetchList.rejected, (state, action) =>{
+    state.loading= false
+    state.error = action.payload as string;
+})
+},
+});
+export const { lists } = mainSlice.actions
+export default mainSlice.reducer
