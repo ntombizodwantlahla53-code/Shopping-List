21aac8eb7ce8cf408881d98aaa84aebab37b6d89
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-08-26 15:50:55 +0200

    added itemReducer and imported it to store

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from '../components/Features/login'
 import registerReducer from '../components/Features/register'
 import mainReducer from '../components/Features/list'
+import itemsReducer from '../components/Features/items'
 
 export const store = configureStore({
     reducer : {
         register : registerReducer,
         login : loginReducer,
         main : mainReducer,
+        items: itemsReducer
     }
 })
 
